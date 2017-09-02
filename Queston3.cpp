55564c6baa7e74982f3021cc255f78ca7815bb51
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-09-02 11:58:48 +0530

    Create Queston3.cpp

---
+++ Queston3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+	int number;
+ cout<<"Enter a number : ";
+ cin>>number;
+ if(number>0)
+   cout<<"Number is Positive";
+ else if(number<0) 
+   cout<<"Number is Negative";
+ else
+   cout<<"Number is Zero";
+ 
+ 
+	return 0;
+}
