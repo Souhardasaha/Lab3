81561f5150e754839fa88ea19e6a3204b582a462
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-09-02 11:47:00 +0530

    Create Question1.cpp

---
+++ Question1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+	int a, b, big;
+	cout<<"Enter two number : ";
+	cin>>a>>b;
+	if(a>b)
+	{
+		big=a;
+	}
+	else
+	{
+		big=b;
+	}
+	cout<<"Biggest of the two number is "<<big;
+ 
+ 
+	return 0;
+}
