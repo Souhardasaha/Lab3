bd2d3d4701e5fc43a2101c7b8602a9d8b0f41dad
Souharda Saha <31206017+Souhardasaha@users.noreply.github.com>
2017-09-06 23:30:40 +0530

    Update Question34.cpp

--- Question34.cpp
+++ Question34.cpp
@@ -1,19 +1,17 @@
-#include <iostream>
-using namespace std;
- 
-int main()
+int main() {
+int a,i,f,l;
+cout << "Enter number : ";
+cin >> a;
+f = a;
+i = 0;
+while(f>10)
 {
-    int a = 5, b = 10, temp;
- 
-    cout << "Before swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
- 
-    temp = a;
-    a = b;
-    b = temp;
- 
-    cout << "\nAfter swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
- 
-    return 0;
+     f = f / 10;
+     i++;
+}
+l = a % 10;
+a = a - l + f;
+a = (a - f*(pow(10,i))) + l*(pow(10,i));
+cout << a;
+return 0;
 }
